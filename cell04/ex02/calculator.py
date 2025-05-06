0056d05e1ce0992580acabbea96f6436ad5f4d28
plunnare <66010645@kmitl.ac.th>
2025-05-06 15:53:48 +0700

    submit ex02

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+a = int(input("Give me the first number: "))
+b = int(input("Give me the second number: "))
+plus = a+b
+substact = a-b
+multiply = a*b
+divide = a/b
+
+print("Thank you!")
+print(a,'+',b,'=',plus)
+print(a,'-',b,'=',substact)
+print(a,'/',b,'=',int(divide))
+print(a,'*',b,'=',multiply)
