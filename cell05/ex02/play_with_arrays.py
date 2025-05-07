2619b9de61d24dde687156d82bcd40bea1a2c546
plunnare <66010645@kmitl.ac.th>
2025-05-07 14:10:11 +0700

    submit ex02

--- cell05/ex02/play_with_arrays.py
+++ cell05/ex02/play_with_arrays.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
+
 a = [2, 8, 9, 48, 8, 22, -12, 2]
-b = [10, 11, 50, 10, 24]
-print(a)
-print(b)
+b = [] 
+for i in a :
+    if i > 5 :
+      b.append( i + 2)
+print( a )
+print( b )
